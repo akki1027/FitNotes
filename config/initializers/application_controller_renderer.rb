1d9e895c9dc92fb472ed6af23ec061096e4fc850
akki1027 <akki10271995@gmail.com>
2020-06-27 08:51:06 +0000

    マイページ

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '6d1cdc56a60dee253d3e8d3ac26636ee7173b3942ca41ddb0667e08209d0c1def93b8512ec80117c0ba5b2f53e650da91f89504979d208a16669d40f9631983e'
